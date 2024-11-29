42de00b450035ff50037c02bb3f8aeed2bc00447
egor_mamontov_2 <egor294710@ya.ru>
2024-11-30 00:10:25 +0300

    добавил цветные логи и оформление

--- src/config.py
+++ src/config.py
@@ -1,18 +1,51 @@
 import os
 import sys
+from colorama import Fore, Style
+import logging
 
+# Корневая папка
 root_dir = os.path.dirname(os.path.abspath(__file__))
 
+
+# URL базы данных
 DATABASE_URL = "postgresql://postgres@127.0.0.1:5432/citymod_feedback"
 
+
+# Путь к базе с файлами
 FILEBASE_PATH = "/home/teoblow/Programs/CityMod-FeedBackService/filebase"
+
+
+# Поддерживаемые расширения
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+
+
+# Класс ловитель ошибок
+class UnauthorizedError(Exception):
+    pass
+
+
+# Логгер и его форматеры
+class ColorFormatter(logging.Formatter):
+    COLORS = {
+        'DEBUG': Fore.WHITE,
+        'INFO': Fore.GREEN,
+        'WARNING': Fore.YELLOW,
+        'ERROR': Fore.RED,
+        'CRITICAL': Fore.MAGENTA
+    }
+
+    def format(self, record):
+        log_color = self.COLORS.get(record.levelname, Style.RESET_ALL)
+        record.levelname = f"{log_color}{record.levelname}{Style.RESET_ALL}"
+        record.msg = f"{log_color}{record.msg}{Style.RESET_ALL}"
+        return super().format(record)
 
 logger_config = {
     "version": 1,
     "disable_existing_loggers": False,
     "formatters": {
         "base": {
+            "()": ColorFormatter,
             "format": "%(levelname)s | %(name)s | %(filename)s | %(asctime)s | %(lineno)s | %(message)s",
             "datefmt": "%H:%M:%S"
         }
